01c7d4b229445c4b73a0af42b4ca1f0b0ceb34e0
Michal <michal.wojcik.programista@gmail.com>
2023-07-14 14:29:12 +0200

    Fix empty rectangle function

--- 052.py
+++ 052.py
@@ -6,33 +6,40 @@ width = input("Podaj szerokość prostokąta: ")
 width = int(width)
 height = input("Podaj wysokość prostokąta: ")
 height = int(height)
+print("\n")
 
 FIRST_LINE = 0
 LAST_LINE = height-1
 FIRST_ROW = 0
 LAST_ROW = width-1
 
-print("Prostokąt pusty: ")
-for i in range(height):
-    if i == FIRST_LINE:
-        for x in range(width):
-            print("*", end=" ")
-        print()
-    if i == LAST_LINE:
-        for x in range(width):
-            print("*", end=" ")
-    if (i > FIRST_LINE and i < LAST_LINE):
-        for x in range(width):
-            if x == FIRST_ROW:
-                print("*", end="")
-            if (x > FIRST_ROW and x < LAST_ROW):
-                print(" ", end=" ")
-            if x == LAST_ROW:
-                print(" *")
-
-print("\n")
 print("Prostokąt pełny: ")
 for i in range(height):
     for x in range(width):
-        print("*", end=" ")
+        print("*", end="  ")
     print()
+
+print("\n")
+print("Prostokąt pusty: ")
+for i in range(height):
+    if i == FIRST_LINE:
+        for x in range(width):            
+            print("*", end="  ")
+        print()
+    if i == LAST_LINE:
+        for x in range(width):
+            if height == 1:
+                break
+            else:
+                print("*", end="  ")
+    if (i > FIRST_LINE and i < LAST_LINE):
+        for x in range(width):            
+            if x == FIRST_ROW:
+                print("*", end="")
+            if (x > FIRST_ROW and x < LAST_ROW):
+                print(" ", end="  ")
+            if x == LAST_ROW:                
+                if width == 1:
+                    print()
+                else:
+                    print("  *")
