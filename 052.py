1e350e4fa50c51137da0dd52e43b8ee263f7ccf9
Michal <michal.wojcik.programista@gmail.com>
2023-07-14 12:59:47 +0200

    Add 052.py file with new finished homework

---
+++ 052.py
@@ -0,0 +1,38 @@
+'''
+Narysuj prostokąt o podanych przez użytkownika rozmiarach.
+'''
+
+width = input("Podaj szerokość prostokąta: ")
+width = int(width)
+height = input("Podaj wysokość prostokąta: ")
+height = int(height)
+
+FIRST_LINE = 0
+LAST_LINE = height-1
+FIRST_ROW = 0
+LAST_ROW = width-1
+
+print("Prostokąt pusty: ")
+for i in range(height):
+    if i == FIRST_LINE:
+        for x in range(width):
+            print("*", end=" ")
+        print()
+    if i == LAST_LINE:
+        for x in range(width):
+            print("*", end=" ")
+    if (i > FIRST_LINE and i < LAST_LINE):
+        for x in range(width):
+            if x == FIRST_ROW:
+                print("*", end="")
+            if (x > FIRST_ROW and x < LAST_ROW):
+                print(" ", end=" ")
+            if x == LAST_ROW:
+                print(" *")
+
+print("\n")
+print("Prostokąt pełny: ")
+for i in range(height):
+    for x in range(width):
+        print("*", end=" ")
+    print()
